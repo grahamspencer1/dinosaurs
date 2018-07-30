1201b54cb3be88f53f49c27c9155292918a63773
Graham Spencer <grahamspence@gmail.com>
2018-07-30 17:54:07 -0400

    exercise complete

--- exercise.rb
+++ exercise.rb
@@ -16,8 +16,13 @@ SELECT name FROM dinos WHERE period='Cretaceous' OR period='Jurassic' ORDER BY n
 
 # Saurischians are the "lizard hipped" order of dinosaurs, and one of the two main branches. All carnivorous dinosaurs are Saurischians, but not all Saurischians are carnivorous. Find all the dinosaurs from the t_order Saurischia that are Herbivorous.
 
+SELECT name FROM dinos WHERE t_order='Saurischia' AND diet='Herbivorous' ORDER BY name ASC;
 
 # Dinosaur names are hard to remember. Find the shortest dinosaur, and rename it Shortie.
 
+UPDATE dinos SET name='Shortie' WHERE length=(SELECT MIN(length) FROM dinos);
+SELECT name FROM dinos WHERE length=(SELECT MIN(length) FROM dinos);
 
 # It's the first day of Dino School, and we're doing roll call. Find the alphabetically first dinosaur, so we can make sure they're present for class.
+
+SELECT name FROM dinos ORDER BY name ASC LIMIT 1;
